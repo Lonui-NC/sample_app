cc6a0133c6db2751f641e6662bdc3219b0b07bb7
Nigel Zhang <congpang1201@163.com>
2016-07-31 15:56:17 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,16 @@ Rails.application.routes.draw do
  #auto complete routes
 
  resources :users
+ resources :sessions,only: [:new,:create,:destroy]
+
+
  root to: 'static_pages#home'
  match '/help', to: 'static_pages#help',  via: 'get'
  match '/about', to: 'static_pages#about',  via:'get'
  match '/contact', to: 'static_pages#contact',  via:'get'
  match '/signup', to: 'users#new',  via:'get'
+ match '/signin',to:'sessions#new',  via:'get'
+ match '/signout',to:'sessions#destroy',  via:'delete'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,6 +32,14 @@ describe "UserPages" do
   		it "should not create a user" do
   			expect {click_button submit}.not_to change(User,:count)
   		end
+
+  		describe "after submission" do
+			before {click_button submit}
+
+			it{should have_title('Sign up')}
+			it{should have_content('error')}
+		end
+
   	end
 
   	describe "with valid information" do
@@ -45,30 +53,28 @@ describe "UserPages" do
 		it "should create a user" do
 			expect{click_button submit}.to change(User, :count).by(1)
 		end
+
+
+		describe "afeter saving the user" do
+			before {click_button submit}
+			let(:user) {User.find_by(email: 'user@example.com')}
+
+			it {should have_link("Sign out")}
+			it{should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+		end
+
+		
+
+
+
+
+
+
 	end
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
+
+
+
+
