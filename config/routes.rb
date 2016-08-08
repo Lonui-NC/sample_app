452820acae8c42ace2676f606cf1ba4b0e790282
Nigel Zhang <congpang1201@163.com>
2016-08-08 21:43:31 +0800

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,16 @@ Rails.application.routes.draw do
 
  #auto complete routes
 
- resources :users
+ resources :users do
+  member do
+    get :following,:followers
+  end
+ end
+
  resources :sessions,only: [:new,:create,:destroy]
  resources :microposts, only: [:create,:destroy]
+ resources :relationships,only:[:create,:destroy]
+
 
  root to: 'static_pages#home'
  match '/help', to: 'static_pages#help',  via: 'get'
